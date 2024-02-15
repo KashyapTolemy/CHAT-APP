dbe2cf43153a66c8edc0e41bdb704654e7a2ab0a
Tolemy Kashyap <kashyaptolemy@gmail.com>
2024-02-15 22:19:09 +0530

    home page almost complete

--- public/chatapp/src/pages/Home/Home.jsx
+++ public/chatapp/src/pages/Home/Home.jsx
@@ -91,16 +91,18 @@ const Home = () => {
                         </div>
                         <div className={styles.home_subtagline_container}>
 
-                            <p className={styles.home_subtagline}>Where Communication Comes Alive. Connect Seamlessly and Explore the Dynamics of Conversation!<br />Join the Chatify community today.</p>
-                            <Link to="/chat" className={styles.home_getstarted} >Get Started</Link>
+                            <div className={styles.home_subtagline}>Where Communication Comes Alive. Connect Seamlessly and Explore the Dynamics of Conversation!<br />Join the Chatify community today.</div>
+                            <Link to="/register" className={styles.home_getstarted} >Get Started</Link>
                         </div>
                     </div>
                     <div className={styles.home_page2}>
                         <div className={styles.home_welcome}>
-                            <p>WELCOME TO PROJECT <br /><span className={styles.home_brandname}>CHATIFY</span></p>
+                        <img src="./stickers/sitting3.png" alt="" className={styles.home_s1_sticker} />
+                            <p className={styles.home_page2title}>WELCOME TO PROJECT <br /><span className={styles.home_brandname}>CHATIFY</span></p>
                         </div>
                         <div className={styles.home_twoparas}>
                             <div className={styles.home_paracontainer}>
+                                
                                 <div className={styles.home_problempara}>
                                     <div className={styles.para_title}>PROBLEM</div>
                                     In the world of online communication, finding a centralized space to connect with friends, family, and colleagues without the hassle of juggling between apps poses a significant challenge.
@@ -116,6 +118,44 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className={styles.home_page3}>
+                        <div className={styles.home_page3title}>OUR CHATIFY IS FOR YOU, IF YOU ARE .</div>
+                        <div className={styles.home_page3container}>
+                            <div className={styles.home_box1container}>
+                                <img src="./stickers/calendar.png" className={styles.box1sticker} />
+                                <div className={styles.home_boxtitle}>15-40 YEARS</div>
+                                <div className={styles.home_boxcontent}>Our Chatify is perfectly fit if you are aged 15 to 40 years.</div>
+                            </div>
+                            <div className={styles.home_box2container}>
+                            <img src="./stickers/comfort.png" className={styles.box2sticker} />
+                                <div className={styles.home_boxtitle}>COMFORT LOVER</div>
+                                <div className={styles.home_boxcontent}>Everything is always simple and easily accessible in Chatify.</div>
+                            </div>
+                            <div className={styles.home_box3container}>
+                            <img src="./stickers/online.png" className={styles.box3sticker} />
+                                <div className={styles.home_boxtitle}>ALWAYS ONLINE</div>
+                                <div className={styles.home_boxcontent}>Staying informed and connected is critical for utilizing messaging efficiently.</div>
+                            </div>
+                            {/* <div className={styles.home_box4container}>
+                                <div className={styles.home_boxtitle}>YOU ARE INTERESTED IN ART</div>
+                                The main idea of Chatify is to make
+                            </div> */}
+                            <div className={styles.home_box4container}>
+                            <img src="./stickers/art.png" className={styles.box4sticker} />
+                                <div className={styles.home_boxtitle}>ACTIVE AND CREATIVE PERSONALITY</div>
+
+                                <div className={styles.home_boxcontent}>Chatify is for creative people, because we believe that a creative approach and active lifestyle are redefining online chatting.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.footer}>
+                        <div className={styles.footer_para}>
+                        <div>&copy;2023 Chatify &#8212;&nbsp;</div><div>Deployed on Vercel &#8212;&nbsp;</div><div>Design and Dev by Tolemy Kashyap &#8212;&nbsp; </div><a href="https://github.com/KashyapTolemy/CHAT-APP" target="_blank" rel="noopener noreferrer">Source Code&nbsp; </a> &#8212; <a href="mailto:kashyaptolemy@gmail.com" target="_blank" rel="noopener noreferrer">&nbsp;Contact</a>
+                        </div>
+                        {/* <div className={styles.footerlogo_container}>
+                        <img src="images/logo1.png" alt ="" className={styles.footerlogo}/>
+                        </div> */}
                     </div>
                 </div>
             )
